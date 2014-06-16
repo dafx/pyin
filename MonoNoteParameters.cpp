3b7bc79a8434c44f1a92a0f1fe5c5b83b684e70f
Matthias Mauch <matthiasm@dcs.qmul.ac.uk>
2014-06-16 21:44:15 +0100

    I think this delivers better note segmentation

--- MonoNoteParameters.cpp
+++ MonoNoteParameters.cpp
@@ -20,10 +20,10 @@ MonoNoteParameters::MonoNoteParameters() :
     nSPP(4), // states per pitch
     n(0),
     initPi(0), 
-    pAttackSelftrans(0.5),
+    pAttackSelftrans(0.99),
     pStableSelftrans(0.999),
     pStable2Silent(0.005),
-    pSilentSelftrans(0.5), 
+    pSilentSelftrans(0.9999), 
     sigma2Note(0.7),
     maxJump(13),
     pInterSelftrans(0.99),
@@ -31,8 +31,8 @@ MonoNoteParameters::MonoNoteParameters() :
     priorWeight(0.5),
     minSemitoneDistance(.5),
     sigmaYinPitchAttack(5), 
-    sigmaYinPitchStable(0.9),
-    sigmaYinPitchInter(5),
+    sigmaYinPitchStable(0.8),
+    sigmaYinPitchInter(.1),
     yinTrust(0.1)
 {
     // just in case someone put in a silly value for pRelease2Unvoiced
